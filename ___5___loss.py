571c64a6abf1b70924400f7416c6671908bd610b
Zora-zjj <54018191+Zora-zjj@users.noreply.github.com>
2020-07-05 20:16:04 +0800

    Update ___5___loss.py

--- ___5___loss.py
+++ ___5___loss.py
@@ -14,21 +14,21 @@ class NLLLoss(nn.Module):
         super(NLLLoss, self).__init__()
         self.weight = weight
 
-    def forward(self, prob, target):
+    def forward(self, prob, target):   #这两者之间的loss计算方式
         """
         Args:
-            prob: (N, C) 
-            target : (N, )
+            prob: (N, C)    # N行C列的矩阵
+            target : (N, )  # N行一列
         """
         N = target.size(0)
         C = prob.size(1)
-        weight = Variable(self.weight).view((1, -1))
+        weight = Variable(self.weight).view((1, -1))   # [1,C]  还是[N,1],应该前者
         weight = weight.expand(N, C)  # (N, C)
         if prob.is_cuda:
             weight = weight.cuda()
-        prob = weight * prob
+        prob = weight * prob     # (N, C)*(N, C)？？？
 
-        one_hot = torch.zeros((N, C))
+        one_hot = torch.zeros((N, C))  # (N, C)全0
         if prob.is_cuda:
             one_hot = one_hot.cuda()
         one_hot.scatter_(1, target.data.view((-1,1)), 1)
@@ -37,7 +37,7 @@ class NLLLoss(nn.Module):
         if prob.is_cuda:
             one_hot = one_hot.cuda()
         loss = torch.masked_select(prob, one_hot)
-        return -torch.sum(loss)
+        return -torch.sum(loss)  # loss矩阵内的每个词相加
 
 
 
